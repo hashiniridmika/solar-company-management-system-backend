875123a69c6eb446cacf19fd037388a177ce91b5
Hashini <hashiniridmika238@gmail.com>
2023-03-29 10:15:50 +0530

    Order item with quantity model is created

---
+++ Models/orderitem_with_quantity_model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Order = require("./order_model");
+const ProductItem = require("./product_item_model");
+
+const orderItemWithQuantitySchema = new mongoose.Schema(
+  {
+    orderId: {
+      type: Schema.types.objectID,
+      ref: "Order",
+    },
+    item: {
+      type: Schema.types.objectID,
+      ref: "ProductItem",
+    },
+    quantity: {
+      type: Number,
+    },
+    price: {
+      type: Number,
+    },
+  },
+  { collation: "orderItemWithQuantities" }
+);
+
+module.exports = model("OrderItemsWithQuantity", orderItemWithQuantitySchema);
