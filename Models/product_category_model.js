ab607d515e8373ae8607809c0a72aa80830112fc
Hashini <hashiniridmika238@gmail.com>
2023-03-29 10:19:02 +0530

    Product model is created

---
+++ Models/product_category_model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const productCategorySchema = new mongoose.Schema(
+  {
+    categoryName: {
+      type: String,
+    },
+    categoryImage: {
+      type: String,
+    },
+    categoryDescription: {
+      type: String,
+    },
+  },
+  { collection: "productCategories" }
+);
+
+module.exports = model("ProductCategory", productCategorySchema);
