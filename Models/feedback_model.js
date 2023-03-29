994efc2c2b79bf11bf3c4e106fb82cc189fe161a
Hashini <hashiniridmika238@gmail.com>
2023-03-29 10:12:50 +0530

    Feedback model is created

---
+++ Models/feedback_model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Agent = require("./ajent_user_model");
+const Order = require("./order_model");
+
+const feedbackSchema = new mongoose.Schema(
+  {
+    agentID: {
+      type: Schema.types.objectID,
+      ref: "Agent",
+    },
+    OrderID: {
+      type: Schema.types.objectID,
+      ref: "Order",
+    },
+    feedback: {
+      type: String,
+    },
+    ratings: {
+      type: String,
+    },
+    feedbackDateandTime: {
+      type: String,
+    },
+  },
+  { collation: "feedbacks" }
+);
+
+module.exports = model("Feedback", feedbackSchema);
