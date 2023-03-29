fa767862faccac3d351559991176212480affe95
Hashini <hashiniridmika238@gmail.com>
2023-03-29 10:09:41 +0530

    Agent user model is created

---
+++ Models/ajent_user_model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const agentSchema = new mongoose.Schema(
+  {
+    agentName: {
+      type: String,
+    },
+    username: {
+      type: String,
+    },
+    password: {
+      type: String,
+    },
+    emailAddress: {
+      type: String,
+    },
+    mobileNumber: {
+      type: String,
+    },
+    companyName: {
+      type: String,
+    },
+    companyAddress: {
+      type: String,
+    },
+  },
+  { collation: "agents" }
+);
+
+module.exports = model("Agent", agentSchema);
