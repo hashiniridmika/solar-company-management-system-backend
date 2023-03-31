096e656afbbd69de10c3655d636775af210265c4
Hashini <hashiniridmika238@gmail.com>
2023-03-31 16:50:23 +0530

    Admin route endpoint is created

---
+++ routes/admin_route.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const adminRoute = express.Router();
+const Admin = require("../models/admin_user_model");
+
+adminRoute.route("/create").post((req, res) => {
+  const { adminName, emailAddress, username, password } = req.body;
+
+  const admin = new Admin({ adminName, emailAddress, username, password });
+  admin // Save admin user details.
+    .save()
+    .then((admin) => {
+      res.send({ status: "success", admin });
+    })
+    .catch((e) => {
+      res.send({ status: "failure" });
+    });
+});
+
+module.exports = adminRoute;
